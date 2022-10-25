b6831c62f58db2cbcd333fe82d3285d00f4ea3e7
Dmitry Shemetov <dshemetov@ucdavis.edu>
2022-10-25 12:46:56 -0700

    Update CLI commands

--- code/forecaster.py
+++ code/forecaster.py
@@ -2,14 +2,14 @@ from os import system
 
 import typer
 
-app = typer.Typer()
+app = typer.Typer(name="flu-hosp-forecaster", chain=True)
 
-@app.command()
+@app.command("forecast")
 def make_forecasts():
     """Make forecasts for our main model."""
     system("Rscript train_model.R")
 
-@app.command()
+@app.command("notebook")
 def make_notebook():
     """Make a notebook for our main model."""
     system("Rscript render_prediction_cards.R")
