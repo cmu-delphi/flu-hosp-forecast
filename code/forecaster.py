b1b20fcf7a88c50d4e58d371e2070f95928821df
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-02-01 13:09:24 -0800

    Runner: switch to rpy2 from os.system

--- code/forecaster.py
+++ code/forecaster.py
@@ -1,35 +1,59 @@
-from os import environ, system
+import os
 
+import rpy2.robjects as robjects
 import typer
 
 app = typer.Typer(name="flu-hosp-forecaster", chain=True)
+CLEAR_R_STATE = "rm(list=ls())"
+
+
+def run_r_script(script_name: str, clear_r_session: bool = True):
+    """Run an R script."""
+    try:
+        robjects.r.source(script_name)
+    except Exception as e:
+        raise typer.Abort(f"Error running {script_name}: {e}")
+    finally:
+        if clear_r_session:
+            robjects.r(CLEAR_R_STATE)
+
+
+def run_r_command(command: str, clear_r_session: bool = True):
+    """Run an R command."""
+    try:
+        robjects.r(command)
+    except Exception as e:
+        raise typer.Abort(f"Error running {command}: {e}")
+    finally:
+        if clear_r_session:
+            robjects.r(CLEAR_R_STATE)
 
 
 @app.command("forecast")
 def make_forecasts(production: bool = False):
     """Make forecasts for our main model."""
     if production:
-        environ["FLU_CACHE"] = "production"
+        os.environ["FLU_CACHE"] = "production"
 
-    system("Rscript train_model.R")
+    run_r_script("train_model.R")
 
 
 @app.command("postprocess")
 def postprocess_forecasts(production: bool = False):
     """Postprocess forecasts for our main model."""
     if production:
-        environ["FLU_CACHE"] = "production"
+        os.environ["FLU_CACHE"] = "production"
 
-    system("Rscript postprocess_forecasts.R")
+    run_r_script("postprocess_forecasts.R")
 
 
 @app.command("notebook")
 def make_notebook(production: bool = False):
     """Make a notebook for our main model."""
     if production:
-        environ["FLU_CACHE"] = "production"
+        os.environ["FLU_CACHE"] = "production"
 
-    system("Rscript render_prediction_cards.R")
+    run_r_script("render_prediction_cards.R")
 
 
 if __name__ == "__main__":
