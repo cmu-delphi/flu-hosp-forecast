8ef4f5f5e913d65020c2707e2ed660ac16a47930
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-01-10 17:57:22 -0800

    refactor: simplify run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-Forecaster runner utility.
+Forecaster Runner Utility
 
 See python run.py --help for usage.
 
@@ -12,11 +12,19 @@ which should be the most recent next Wednesday (it is also the effective as of
 date for requesting API data). The reference date is the Saturday after the due
 date.
 
-We assume that the submission repo path is set in the environment variable
-FLU_SUBMISSIONS_PATH. The submission repo for the 2023-2024 season is at
+To generate forecasts simply run:
 
-    https://github.com/cdcepi/FluSight-forecast-hub/
+    python run.py forecast
 
+After setting geo exclusions in run.R, you can run the same command again to
+generate excluded forecasts.
+
+Necessary environment variables (set in .env):
+
+    * FLU_SUBMISSIONS_DIR is the submission repo for the Flu Forecast Hub,
+      which you can clone from https://github.com/cdcepi/FluSight-forecast-hub/
+    * SLACK_BOT_TOKEN is the token for the Slack bot that will post the
+      forecasts to Slack.
 """
 
 import os
@@ -28,7 +36,7 @@ from pathlib import Path
 
 import git
 import typer
-from dotenv import load_dotenv, set_key
+from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
@@ -59,20 +67,6 @@ def get_previous_weekday(cur_date: datetime, weekday: int) -> datetime:
     return cur_date - timedelta((7 - weekday + cur_date.weekday()) % 7)
 
 
-def check_exists(path: Path):
-    if not path.exists():
-        raise FileNotFoundError(f"{path} not found.")
-
-
-def check_and_set_var(key: str, msg: str, force: bool = False):
-    if not os.environ.get(key) or force:
-        print(f"Your {key} env var is not set.")
-        path = input(f"{msg}")
-        check_exists(Path(path))
-        set_key(".env", key, path, export=True)
-
-
-DATE_FORMAT = "%Y-%m-%d"
 FORECAST_GENERATION_DATE = datetime.today()
 FORECAST_DUE_DATE = get_next_weekday(FORECAST_GENERATION_DATE, 2)
 REFERENCE_DATE = get_next_weekday(FORECAST_DUE_DATE, 5)
@@ -86,19 +80,15 @@ FLU_PREDICTIONS_NOTEBOOK = (
     / "data-forecasts"
     / f"{REFERENCE_DATE:%Y-%m-%d}-flu-forecast.html"
 )
-if not os.environ.get("FLU_SUBMISSIONS_PATH"):
-    check_and_set_var(
-        "FLU_SUBMISSIONS_PATH",
-        "Please enter the path to 'Flusight-forecast-data' (e.g. /Users/username/Documents/Flusight-forecast-data): ",
-    )
-FLU_SUBMISSION_PATH = (
-    Path(os.environ.get("FLU_SUBMISSIONS_PATH", "")) / "model-output" / "CMU-TimeSeries"
+if not os.environ.get("FLU_SUBMISSIONS_DIR"):
+    raise ValueError("FLU_SUBMISSIONS_DIR not found. See script docstring for info.")
+FLU_SUBMISSION_DIR = (
+    Path(os.environ.get("FLU_SUBMISSIONS_DIR", "")) / "model-output" / "CMU-TimeSeries"
 )
-if not os.environ.get("SLACK_BOT_TOKEN"):
-    check_and_set_var(
-        "SLACK_BOT_TOKEN",
-        "Please enter your Slack bot token: ",
-    )
+if not FLU_PREDICTIONS_FILE.exists():
+    raise FileNotFoundError(f"Flu predictions file {FLU_PREDICTIONS_FILE} not found.")
+if not FLU_SUBMISSION_DIR.exists():
+    os.makedirs(FLU_SUBMISSION_DIR)
 
 
 @app.command("forecast")
@@ -107,51 +97,20 @@ def make_forecasts(
         False, "--clear-cache", "-c", help="Clear the caches before making forecasts."
     )
 ):
-    """Make flu forecasts for the most recent Wednesday.
-
-    Writes to data-forecasts/CMU-TimeSeries/.
-    """
+    """Make flu hospitalization forecasts."""
     if clear_cache:
         os.environ["FLU_HOSP_CLEAR_CACHE"] = "TRUE"
     subprocess.run(["Rscript", "run.R"], check=True)
 
 
-@app.command("set-vars")
-def set_vars(force: bool = False):
-    """Set environment variables for the utility."""
-    print(
-        "Checking and setting environment variables... (press Enter to skip any variable)"
-    )
-    check_and_set_var(
-        "FLU_SUBMISSIONS_PATH",
-        "Please enter the path to 'Flusight-forecast-data' (e.g. /Users/username/Documents/Flusight-forecast-data): ",
-        force=force,
-    )
-    check_and_set_var(
-        "SLACK_BOT_TOKEN", "Please enter your Slack bot token: ", force=force
-    )
-
-
 def copy_to_repo():
-    """Copy predictions to the submission repo.
-
-    The repo path is specified in the env var FLU_FORECASTER_PATH.
-    """
-    check_exists(FLU_PREDICTIONS_FILE)
-    if not FLU_SUBMISSION_PATH.exists():
-        os.makedirs(FLU_SUBMISSION_PATH)
-    shutil.copy(FLU_PREDICTIONS_FILE, FLU_SUBMISSION_PATH)
+    """Copy predictions to the submission repo."""
+    shutil.copy(FLU_PREDICTIONS_FILE, FLU_SUBMISSION_DIR)
 
 
 def get_latest_commit_date(repo: git.Repo) -> datetime:
-    """Search for the last submission by the CMU-TimeSeries group and extract the date.
-
-    Assumes the CMU-TimeSeries group uses the following commit message format:
-
-        [CMU-TimeSeries] Add YYYY-MM-DD predictions
-    """
+    """Search for the last submission by the CMU-TimeSeries group and extract the date."""
     pattern = r"\[CMU-TimeSeries\] Add (\d+-\d+-\d+) predictions"
-
     for commit in repo.iter_commits():
         if date_match := re.search(pattern, commit.message):
             return datetime.strptime(date_match.groups()[0], "%Y-%m-%d")
@@ -172,36 +131,30 @@ def switch_to_branch(repo: git.Repo, branch_name: str):
 
 
 def commit_to_repo():
-    """Commit to submission repo.
-
-    The repo path is specified in the env var FLU_FORECASTER_PATH.
+    """Commit to the submission repo.
 
     Can't use git.index because we're using a sparse index. So we use the CLI
     wrapper in git instead.
     """
-    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_PATH"])
+    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_DIR"])
     assert flu_submissions_repo.remote(name="origin").exists()
-
     latest_commit_date = get_latest_commit_date(flu_submissions_repo)
     if latest_commit_date < REFERENCE_DATE:
         flu_submissions_repo.git.add(
-            str(FLU_SUBMISSION_PATH / f"{REFERENCE_DATE:%Y-%m-%d}-CMU-TimeSeries.csv")
+            str(FLU_SUBMISSION_DIR / f"{REFERENCE_DATE:%Y-%m-%d}-CMU-TimeSeries.csv")
         )
         flu_submissions_repo.git.commit(
             "-m", f"[CMU-TimeSeries] Add {REFERENCE_DATE:%Y-%m-%d} predictions"
         )
     else:
         print(
-            f"Latest commit is for {latest_commit_date.strftime(DATE_FORMAT)}; not committing."
+            f"Latest commit is for {latest_commit_date.strftime('%Y-%m-%d')}; not committing."
         )
 
 
 def push_to_repo():
-    """Push to the submission remote.
-
-    The repo path is specified in the env var FLU_FORECASTER_PATH.
-    """
-    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_PATH"])
+    """Push to the submission repo."""
+    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_DIR"])
     assert flu_submissions_repo.remote(name="origin").exists()
     branch_name = flu_submissions_repo.active_branch.name
 
@@ -211,15 +164,11 @@ def push_to_repo():
 
 
 @app.command("submit")
-def submit():
-    """Copy, commit, and push to the submission repo.
-
-    The repo path is specified in the env var FLU_FORECASTER_PATH.
-    """
-    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_PATH"])
+def submit_forecasts():
+    """Copy, commit, and push to the submission repo."""
+    flu_submissions_repo = git.Repo(os.environ["FLU_SUBMISSIONS_DIR"])
     assert flu_submissions_repo.remote(name="origin").exists()
     assert flu_submissions_repo.active_branch.name == "main"
-
     flu_submissions_repo.remote(name="origin").pull()
 
     # make_new_branch(flu_submissions_repo, f"forecast-{REFERENCE_DATE:%Y-%m-%d}")
@@ -232,59 +181,43 @@ def submit():
     # switch_to_branch(flu_submissions_repo, "main")
 
 
-def upload_file_to_slack(
-    client: WebClient, file_path: str, slack_file_name: str
-) -> str:
-    result = client.files_upload(file=file_path, title=slack_file_name)
-    if not result.get("ok"):
-        raise SlackApiError("Couldn't upload file.", result)
-    else:
-        file_link = result.get("file").get("permalink")
-        return file_link
-
-
-def post_upload_message(client: WebClient, text: str, channel: str):
-    result = client.chat_postMessage(channel=channel, text=text)
-    if not result.get("ok"):
-        raise SlackApiError("Couldn't post message.", result)
-
-
-def get_hyperlink_text(file_link: str, hyperlink_title: str) -> str:
-    """Get the text for a hyperlink in Slack."""
-    return f"<{file_link}|{hyperlink_title}>"
-
-
 @app.command("post-slack")
 def post_notebook_to_slack(test_mode: bool = False):
     """Post the notebook and prediction csv to Slack."""
     if token := os.environ.get("SLACK_BOT_TOKEN"):
         client = WebClient(token=token)
     else:
-        raise ValueError("SLACK_BOT_TOKEN not found.")
+        raise ValueError("SLACK_BOT_TOKEN not found. See script docstring for info.")
 
-    SLACK_CHANNEL = "C03SD5K905D" if test_mode else "C03A46R6LBW"
-    ROLE_TAG_STRING = "<!subteam^S05AN3WH91V|covid-forecast-submission>"
-
-    notebook_link = upload_file_to_slack(
-        client,
-        str(FLU_PREDICTIONS_NOTEBOOK),
-        FLU_PREDICTIONS_NOTEBOOK.name,
+    # Upload notebook
+    result = client.files_upload_v2(
+        file=str(FLU_PREDICTIONS_NOTEBOOK), title=FLU_PREDICTIONS_NOTEBOOK.name
     )
-    csv_link = upload_file_to_slack(
-        client,
-        str(FLU_PREDICTIONS_FILE),
-        FLU_PREDICTIONS_FILE.name,
-    )
+    if not result.get("ok"):
+        raise SlackApiError("Couldn't upload file.", result)
+    else:
+        notebook_link = result.get("file").get("permalink")
 
-    hyperlinks = " ".join(
-        [
-            get_hyperlink_text(notebook_link, "Notebook"),
-            get_hyperlink_text(csv_link, "CSV"),
-        ]
+    # Upload csv
+    result = client.files_upload_v2(
+        file=str(FLU_PREDICTIONS_FILE), title=FLU_PREDICTIONS_FILE.name
     )
-    text = f"Flu: {hyperlinks} for {FORECAST_DUE_DATE:%Y-%m-%d} {ROLE_TAG_STRING}"
+    if not result.get("ok"):
+        raise SlackApiError("Couldn't upload file.", result)
+    else:
+        csv_link = result.get("file").get("permalink")
 
-    post_upload_message(client, text, channel=SLACK_CHANNEL)
+    # Form the message
+    hyperlinks = f"<{notebook_link}|Notebook> <{csv_link}|CSV>"
+    role_tag_string = "<!subteam^S05AN3WH91V|covid-forecast-submission>"
+    text = f"Flu: {hyperlinks} for {FORECAST_DUE_DATE:%Y-%m-%d} {role_tag_string}"
+
+    # Post the message
+    result = client.chat_postMessage(
+        channel="C03SD5K905D" if test_mode else "C03A46R6LBW", text=text
+    )
+    if not result.get("ok"):
+        raise SlackApiError("Couldn't post message.", result)
 
 
 if __name__ == "__main__":
